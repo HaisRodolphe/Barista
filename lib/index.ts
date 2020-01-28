56494e1618b700861af2e1fca8437e94b2c153f5
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-01-28 12:59:41 +0100

    modification index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,10 @@
 function init() {
-  let canvas = document.querySelector("canvas");
+  let canvas = getCanvas();
   console.log(canvas);
+}
+
+function getCanvas() {
+  return document.querySelector("canvas");
 }
 
 window.addEventListener("load", init);
