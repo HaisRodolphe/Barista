9d4960fc9a68cb01bb185e587b186814882d153f
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-01-29 16:45:49 +0100

    Note "index.ts"

--- lib/index.ts
+++ lib/index.ts
@@ -1,15 +1,23 @@
 function init() {
   let canvas = getCanvas();
   let context =getContext();
-
+  // on détermine les dimensions du canvas
   canvas.width=640;
   canvas.height=480;
-  
+  // crée un nouveau path 
   context.beginPath();
+  // on définit l'épaisseur de la ligne (unité en px)
   context.lineWidth=5;
+  // on bouge vers les coordonnées x = 10 et y = 10 (sans rien dessiner)
   context.moveTo(10,10);
+  // on dessine une ligne depuis notre point de départ vers le point d'arrivée (x=10,y100)
   context.lineTo(10,100);
+  // on va donc définir un style de remplissage
   context.strokeStyle="#00ff00";
+  // et enfin on appelle la méthode stroke, qui va se charger d'éxécuter l'ensemble 
+  // des commandes que nous avons utilisé depuis que l'on invoqué beinPath()
+  // si l'on commente l'appel à cette méthode, rien ne se produit. 
+  // L'appel à stroke permet de fermer le chemin crée par beginPath().
   context.stroke();
 }
 
