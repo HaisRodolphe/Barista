4d2761b8a8d930fb4c4f16da724a98530a3c14e9
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-01-29 16:49:12 +0100

    Note "index.ts"

--- lib/index.ts
+++ lib/index.ts
@@ -20,13 +20,22 @@ function init() {
   // L'appel à stroke permet de fermer le chemin crée par beginPath().
   context.stroke();
 }
-
+/*
+* Retourne une référence à l'objet canvas crée à l'aide de la balise 
+* placée dans le code html
+*/
 function getContext(){
   return getCanvas().getContext("2d")
 }
-
+/*
+* Retourne une référence à l'objet canvas crée à l'aide de la balise 
+* placée dans le code html
+*/
 function getCanvas() {
   return document.querySelector("canvas");
 }
-
+/* 
+* Quand toutes les données sont chargées (DOM, images, sons etc...)
+* On démarre l'application par la fonction init
+*/
 window.addEventListener("load", init);
