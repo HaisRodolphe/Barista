9210813a5be848b5a313339a62d5bd3c90fe919c
Hais Rodolphe <44665224+HaisRodolphe@users.noreply.github.com>
2020-01-29 13:08:54 +0100

    Creation ligne et format canvas "index.tx"

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,20 @@
 function init() {
   let canvas = getCanvas();
-  console.log(canvas);
+  let context =getContext();
+
+  canvas.width=640;
+  canvas.height=480;
+  
+  context.beginPath();
+  context.lineWidth=5;
+  context.moveTo(10,10);
+  context.lineTo(10,100);
+  context.strokeStyle="#00ff00";
+  context.stroke();
+}
+
+function getContext(){
+  return getCanvas().getContext("2d")
 }
 
 function getCanvas() {
